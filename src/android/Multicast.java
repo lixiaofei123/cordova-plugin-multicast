c376af9328649cbd7e78ad7f95df47c402af013c
lixiaofei <326256365@qq.com>
2020-10-03 21:36:26 +0800

    beidou

--- src/android/Multicast.java
+++ src/android/Multicast.java
@@ -58,12 +58,8 @@ public class Multicast extends CordovaPlugin {
                         Log.d(TAG, "Waiting for location packet!");
                         socket.receive(packet);
                         String rawData = new String(data, 0, packet.getLength());
-                        Log.d(TAG, "rawData is " + rawData);
                         if(rawData.contains("$BDRMC")){
-                            Log.d(TAG, "have $BDRMC");
                             setLocation(rawData);
-                        }else{
-                            Log.d(TAG, "no have $BDRMC");
                         }
                     }catch(Exception e){
                         Log.d(TAG, "Receive exception:" + e.toString());
@@ -80,25 +76,14 @@ public class Multicast extends CordovaPlugin {
 
         synchronized public void setLocation(String info) {
 
-            Log.d(TAG, "execute setLocation");
-
             String lonStr = degreeModify(info.split(",")[5]);
             String latStr = degreeModify(info.split(",")[3]);
-
-
-            Log.d(TAG, "lonStr is " + lonStr + "  latStr is " + latStr);
 
             double lon = Double.parseDouble(lonStr);
             double lat = Double.parseDouble(latStr);
 
-            Log.d(TAG, "lon is " + lon + "  lat is " + lat);
-
-
             locations[0] = lon;
             locations[1] = lat;
-
-            Log.d(TAG, "locations[0] is " + locations[0] + "   locations[1] is " +  locations[1]);
-
          
           }
 
@@ -108,16 +93,11 @@ public class Multicast extends CordovaPlugin {
 
         public String degreeModify(String rawData) {
 
-            Log.d(TAG, "degreeModify rawData is " + rawData);
-
             String head = rawData.split("\\.")[0];
             String end = rawData.split("\\.")[1];
             double res = Double.parseDouble(head);
             res = res / 100;
             String r = String.valueOf(res);
-
-            Log.d(TAG, "degreeModify result is " + r + end);
-
             return r + end;
           }
 
@@ -170,21 +150,15 @@ public class Multicast extends CordovaPlugin {
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         final int id = data.getInt(0);
         DatagramSocket socket = m_sockets.get(id);
-        Log.d(TAG, "action is " + action);
         if (action.equals("initLocation")){
-            Log.d(TAG, "will initLocation");
             m_locListener = new LocationListener();
             m_locListener.start();
             callbackContext.success();
         }else if(action.equals("getLocation")){
-            Log.d(TAG, "will  getLocation");
             if(m_locListener != null){
                 double[] locations = m_locListener.getLocation();
-                Log.d(TAG, "m_locListener is not  null,location is " + locations[0] + " , " + locations[1] );
-                Log.d(TAG, "["+locations[0] +","+ locations[1] +"]");
                 callbackContext.success("["+locations[0] +","+ locations[1] +"]");
             }else{
-                Log.d(TAG, "m_locListener is null");
                 callbackContext.error("Please call initLocation() to init Location Thread first");
             }
         }else if(action.equals("create")) {
